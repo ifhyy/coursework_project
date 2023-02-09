76a5a0d3285c8f4bac319ec547d80fe05f934ac6
Artem Stupak <bozzya7@gmail.com>
2023-02-09 09:59:25 +0100

    added requirements

--- coursework_project/settings.py
+++ coursework_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+j-_^y$7!$wc_+2c21o75d5nb*osllujuqop&84ft$7gftd1sk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'coursework_project.urls'
@@ -103,6 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
 
 LANGUAGE_CODE = 'en-us'
 
