86f1f83d22344f84847b7ebdedf1866f191647cf
ifhyy <98919761+ifhyy@users.noreply.github.com>
2023-02-28 12:31:49 +0100

    Update settings.py

--- coursework_project/settings.py
+++ coursework_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+j-_^y$7!$wc_+2c21o75d5nb*osllujuqop&84ft$7gftd1sk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
