7f6469efb46bbe6751221c3093259127b585b695
Artem Stupak <bozzya7@gmail.com>
2023-03-02 13:38:46 +0100

    [updt] started using PostgreSQL

--- coursework_project/settings.py
+++ coursework_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'coursework_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'market_db',
+        'USER': 'stupk',
+        'PASSWORD': 'sosipisos',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
